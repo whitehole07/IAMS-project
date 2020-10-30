70bdb36ea140c06a10536bae677f2e5655d00a6a
Daniele Agamennone <danieleagamennone@gmail.com>
2020-10-30 14:20:07 +0100

    Aggiornato handler.py

--- handler.py
+++ handler.py
@@ -26,15 +26,15 @@ class OrbitsHandler(object):
         self.transfers: dict = {}
         for key, value in kwargs.items():
             if type(value) is list and len(value) == 2:
-                self.transfers = {**self.transfers, **{key: TransferAbstract(value[0], value[1])}}
+                self.transfers = {**self.transfers, **{key: {"transfer": TransferAbstract(value[0], value[1])}}}
             elif isinstance(value, TransferAbstract):
-                self.transfers = {**self.transfers, **{key: value}}
+                self.transfers = {**self.transfers, **{key: {"transfer": value}}}
             else:
                 raise IncompatibleObjectError(f"Incompatible object passed: kwarg = '{key}': {type(value)}'")
 
-    def get_comparison_matrix(self, transfer_name: str, *, to_csv: bool = True) -> pd.DataFrame:
+    def get_comparison_matrix(self, transfer_name: str) -> pd.DataFrame:
         try:
-            tr: TransferAbstract = self.transfers[transfer_name]
+            tr: TransferAbstract = self.transfers[transfer_name]["transfer"]
         except KeyError:
             raise KeyError(f"'{transfer_name}' not found in transfers' list")
 
@@ -44,12 +44,47 @@ class OrbitsHandler(object):
                 for where_RE in ["pericentre", "apocentre"]:
                     df.loc[len(df)] = tr.get_combination_array(where_CP, RP[0], RP[1], where_RE)
 
-        if to_csv:
+        self.transfers[transfer_name]["comparison_matrix"] = df
+        return df
+
+    def comparison_matrix_to_file(self, transfer_name: str, *, to_excel: bool = True, single_file: bool = True) -> None:
+        try:
+            tr: TransferAbstract = self.transfers[transfer_name]["transfer"]
+            df: pd.DataFrame = self.transfers[transfer_name]["comparison_matrix"]
+        except KeyError:
+            raise KeyError("Comparison Matrix or TransferAbstract object not founded, you should run get_comparison_matrix function before")
+
+        if not single_file and to_excel:
             if not os.path.exists("csvs"):
                 os.mkdir("csvs")
 
-            df.to_csv("csvs/%s_init(a=%.2f, e=%.2f, i=%.2f)_to_final(a=%.2f, e=%.2f, i=%.2f).csv"
-                      % (transfer_name,
-                         tr.orbit_i.a, tr.orbit_i.e, tr.orbit_i.i,
-                         tr.orbit_f.a, tr.orbit_f.e, tr.orbit_f.i))
-        return df
+            name: str = "%s_init(a=%.2f, e=%.2f, i=%.2f)_to_final(a=%.2f, e=%.2f, i=%.2f)" \
+                        % (transfer_name,
+                           tr.orbit_i.a, tr.orbit_i.e, tr.orbit_i.i,
+                           tr.orbit_f.a, tr.orbit_f.e, tr.orbit_f.i)
+
+            df.to_csv(f"csvs/{name}.csv")
+
+        elif single_file and to_excel:
+            if not os.path.exists("csvs"):
+                os.mkdir("csvs")
+
+            name: str = "%s_init(a=%.2f, e=%.2f, i=%.2f)_to_final(a=%.2f, e=%.2f, i=%.2f)" \
+                        % (transfer_name,
+                           tr.orbit_i.a, tr.orbit_i.e, tr.orbit_i.i,
+                           tr.orbit_f.a, tr.orbit_f.e, tr.orbit_f.i)
+
+            if not os.path.exists(f"csvs/{name}"):
+                os.mkdir(f"csvs/{name}")
+
+            df[:]["dv_tot"].to_excel(f"csvs/{name}/dv_tot.xlsx")
+            df[:]["dt_tot"].to_excel(f"csvs/{name}/dt_tot.xlsx")
+
+            for index, row in df.iterrows():
+                dir_name: str = f"where_CP='{row['where_CP']}'_where_RP='{row['where_RP']}'_type_RP='{row['type_RP']}'_where_RE='{row['where_RE']}'"
+
+                if not os.path.exists(f"csvs/{name}/{dir_name}"):
+                    os.mkdir(f"csvs/{name}/{dir_name}")
+
+                for data in ("dv", "dt"):
+                    pd.DataFrame([d for x in row[f"{data}_list"] for d in x]).to_excel(f"csvs/{name}/{dir_name}/{data}.xlsx")
